8fef230c3bd13f66047fdf1292ed3f9b7796e16c
Gaurav Bajaj <goru97@gmail.com>
2016-01-27 14:37:05 -0800

    Partitions_as_args

--- src/main/java/com/tilogaat/kafka/SimpleExample.java
+++ src/main/java/com/tilogaat/kafka/SimpleExample.java
@@ -25,11 +25,11 @@ public class SimpleExample {
         SimpleExample example = new SimpleExample();
         long maxReads = Long.parseLong(args[0]);
         String topic = args[1];
-        int num_partitions = Integer.parseInt(args[2]);
         List<String> seeds = new ArrayList<String>();
-        seeds.add(args[3]);
-        int port = Integer.parseInt(args[4]);
-        int timeout = Integer.parseInt(args[5]);
+        seeds.add(args[2]);
+        int port = Integer.parseInt(args[3]);
+        int timeout = Integer.parseInt(args[4]);
+        int num_partitions = args.length - 5;
 
         System.out.println("Pulling data from partitions :"+ num_partitions);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
@@ -38,7 +38,9 @@ public class SimpleExample {
         ExecutorService executor = Executors.newFixedThreadPool(num_partitions);
         try {
             for (int i = 0; i < num_partitions; i++) {
-                executor.submit(new SimpleConsumerRunnable(seeds, port, topic, i, i, maxReads));
+                int partition = Integer.parseInt(args[5 + i]);
+                System.out.println("Current Partition: "+partition);
+                executor.submit(new SimpleConsumerRunnable(seeds, port, topic, partition, i, maxReads));
             }
             //example.su(maxReads, topic, partition, seeds, port);
         } catch (Exception e) {
